7fa03ea6f2e43fdebfa97d09eb74bc2d7ac7b7d8
christay <christay@usc.edu>
2021-04-14 01:26:39 +0000

    update-service 1

--- update-service-environment.py
+++ update-service-environment.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import os
 import json
+import shutil
 import subprocess
 
 SERVICENODE = 'MPInode'
@@ -22,19 +23,43 @@ def getnodeIP(i):
       nodeIP = items['IP']
   return(nodeIP)
 
+def createknownhosts(n, k):
+  kh = ['ssh-keyscan', n]
+  khrun = subprocess.run(kh, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout.decode('utf-8')
+  f = open(k, 'a')
+  f.write(str(khrun))
+  f.close
+
+def createnodelist(n, nl):
+  f = open(nl, 'a')
+  f.write(n + '\n')
+  f.close
+
 def main():
   nodeIPlist = []
   serviceshow = ['sudo', 'onegate', 'service', 'show', '--json']
-  serviceresult = subprocess.run(serviceshow, stdout=subprocess.PIPE).stdout.decode('utf-8')
+  serviceresult = subprocess.run(serviceshow, stdout=subprocess.PIPE).stdout
   nodeID = getnodeID(serviceresult)
   for node in nodeID:
     vmshow = ['sudo', 'onegate', 'vm', 'show', node, '--json']
     vmresult = subprocess.run(vmshow, stdout=subprocess.PIPE).stdout.decode('utf-8')
     nodeIPlist.append(getnodeIP(vmresult))
 
+  print('Backing up ~/.ssh/known_hosts if present.')
+  khpath = '~/.ssh/known_hosts'
+  khexppath = os.path.expanduser(khpath)
+  if os.path.isfile(khexppath):
+    shutil.move(khexppath, khexppath + '.bak')
+  nlpath = os.path.expanduser('~/node-list.txt')
+  if os.path.isfile(nlpath):
+    os.remove(nlpath)
+
   print('Discovered IPs for nodes in Service:')
   for nodeIP in nodeIPlist:
     print(nodeIP)
+    createknownhosts(nodeIP, khexppath)
+    createnodelist(nodeIP, nlpath)
+  print('List of nodes updated in ~/node-list.txt')
 
 if __name__ == '__main__':
   main()
